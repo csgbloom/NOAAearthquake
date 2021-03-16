7da547da3fbc4acc22957b0a871fa040bc81f3d8
csgbloom <glaucousbloom@protonmail.com>
2021-03-16 17:28:30 +0000

    Update description and amend test

--- tests/testthat/test-eq_create_label.R
+++ tests/testthat/test-eq_create_label.R
@@ -3,18 +3,18 @@ require(magrittr)
 
 df <- system.file("extdata", "earthquakes.tsv", package = "NOAAearthquake")
 df1 <- eq_clean_data(df)
-df2 <- eq_location_clean(df1) %>% 
+df2 <- eq_location_clean(df1) %>%
   dplyr::filter(Country == "Turkey", lubridate::year(Date) >= 2010)
 
 test_that("eq_create_label returns class character", {
-  mt <- df2 %>% 
+  mt <- df2 %>%
     dplyr::mutate(popup_text = eq_create_label(.))
   testthat::expect_that(mt$popup_text, is_a("character"))
 })
 
 test_that("eq_create_label omits NA values in label", {
-  mt <- df2 %>% 
-    dplyr::mutate(popup_text = eq_create_label(.)) %>% 
-    dplyr::mutate(chk = grepl("NA", mt$popup_text, fixed = TRUE))
-  testthat::expect_true(sum(mt$chk) == 0)
+  mt <- df2 %>%
+    dplyr::mutate(popup_text = eq_create_label(.))
+  chk = grepl("NA", mt$popup_text, fixed = TRUE)
+  testthat::expect_true(sum(chk) == 0)
 })
